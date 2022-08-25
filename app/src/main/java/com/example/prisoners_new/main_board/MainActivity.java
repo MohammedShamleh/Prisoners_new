c51ae22ffcfa8f5928c858a2b185a365aaf072d6
MahmoudOstaz <zahoq2013@gmail.com>
2022-08-25 18:33:43 +0300

    done

--- app/src/main/java/com/example/prisoners_new/main_board/MainActivity.java
+++ app/src/main/java/com/example/prisoners_new/main_board/MainActivity.java
@@ -1,63 +1,14 @@
-package com.example.prisoners_new.main_board;
+package com.example.prisoners_new;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.cardview.widget.CardView;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
-import com.example.prisoners_new.R;
-import com.example.prisoners_new.main_board.cardView.salary;
-import com.example.prisoners_new.main_board.cardView.report;
-
-public class MainActivity extends AppCompatActivity  implements View.OnClickListener  {
-    CardView salary, report, statistic, news;
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initview();
-
-
-
     }
-    public void initview(){
-        salary = findViewById(R.id.salary);
-        salary.setOnClickListener((View.OnClickListener) this);
-
-        report = findViewById(R.id.report);
-        report.setOnClickListener((View.OnClickListener) this);
-
-        statistic = findViewById(R.id.statistic);
-        statistic.setOnClickListener((View.OnClickListener) this);
-
-        news = findViewById(R.id.news);
-        news.setOnClickListener((View.OnClickListener) this);
-    }
-    @Override
-    public void onClick(View view) {
-        Intent i;
-        switch (view.getId()) {
-            case R.id.salary:
-                i = new Intent(getApplicationContext(), salary.class);
-                startActivity(i);
-                break;
-            case R.id.report:
-                i = new Intent(getApplicationContext(), report.class);
-                startActivity(i);
-                break;
-            case R.id.news:
-                i = new Intent(getApplicationContext(),  salary.class);
-                startActivity(i);
-                break;
-            case R.id.statistic:
-                i = new Intent(getApplicationContext(),  salary.class);
-                startActivity(i);
-                break;
-
-        }
-    }
-
 }
